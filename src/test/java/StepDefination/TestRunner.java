01731bffa0932d8049f701250ecca845fd9cf014
Kashif Akhtar <kashifakh224@gmail.com>
2026-06-02 17:44:41 +0530

    Switch runner structure to JUnit to resolve compiler error

--- src/test/java/StepDefination/TestRunner.java
+++ src/test/java/StepDefination/TestRunner.java
@@ -2,13 +2,17 @@ package StepDefination;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
 
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
 	    features = "src/test/resources/features",
 	    glue = "StepDefination", // <-- Fixed the typo here to match line 1
 	    plugin = {
 	        "pretty",
-	        "html:target/cucumber-report.html"
+	        "html:target/cucumber-reports.html"
 	    }
 	)
 public class TestRunner extends AbstractTestNGCucumberTests {
